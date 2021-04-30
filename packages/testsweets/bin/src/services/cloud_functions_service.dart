ef94e7f06429763ea11356e8b8cd06dc898cb214
Batandwa Mgutsi <B.mgutsi@gmail.com>
2021-04-30 16:21:27 +0200

    Fixes automation keys upload error even when the upload was succesful

--- packages/testsweets/bin/src/services/cloud_functions_service.dart
+++ packages/testsweets/bin/src/services/cloud_functions_service.dart
@@ -46,6 +46,6 @@ class _CloudFunctionsService implements CloudFunctionsService {
       'automationKeys': automationKeys,
     });
 
-    if (ret.statusCode != 500) throw ret.body;
+    if (ret.statusCode != 200) throw ret.body;
   }
 }
