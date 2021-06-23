ab3a305f2b303bbfb7de4d8b1f78ef7a5904122f
Ebrahim Soliman <esoliman@nbsventure.com>
2021-06-23 18:04:57 +0300

    fix unit tests names

--- packages/testsweets/test/test_sweets_config_file_service_test.dart
+++ packages/testsweets/test/test_sweets_config_file_service_test.dart
@@ -17,44 +17,47 @@ void main() {
     test(
         "Should throw BuildError if the given app directory does not contain a .testsweets file",
         () async {
-      final mockFileSystemService =
-          getAndRegisterFileSystemService(doesFileExist: false);
+      getAndRegisterFileSystemService(doesFileExist: false);
       final configFileService = TestSweetsConfigFileServiceImplementaion();
 
-      expect(mockFileSystemService.doesFileExist(testDirectoryPath), false);
       expect(
           () => configFileService
               .getValueFromConfigFileByKey(ConfigFileKeyType.ApiKey),
           throwsA(BuildError(ErrorMessages.projectConfigNotCreated)));
     });
-    test(
-        "Should split any list of strings by the '=' sign to List<MapEntry<String, String>>",
-        () async {
-      TestSweetsConfigUtils testSweetsConfigUtils = TestSweetsConfigUtils();
-      var result = testSweetsConfigUtils
-          .splittingStringOnEqualSign(testsweetFileContent);
-      expect(result[0].value, '3OezzTovG9xFTE5Xw2w1');
-    });
-    test(
-        "Should split any list of strings by the '=' sign to List<MapEntry<String, String>>",
-        () async {
-      TestSweetsConfigUtils testSweetsConfigUtils = TestSweetsConfigUtils();
-      var result = testSweetsConfigUtils
-          .splittingStringOnEqualSign(testsweetFileContent);
-      expect(result[0].value, '3OezzTovG9xFTE5Xw2w1');
-    });
-    test("get value from map by key", () async {
-      TestSweetsConfigUtils testSweetsConfigUtils = TestSweetsConfigUtils();
-      var result = testSweetsConfigUtils.getStringValueFromMapByKey(
-          testsweetFileContentListOfMapEntries, ConfigFileKeyType.ProjectId);
-      expect(result, '3OezzTovG9xFTE5Xw2w1');
-    });
-    test("deserialize Config File by key", () async {
-      TestSweetsConfigUtils testSweetsConfigUtils = TestSweetsConfigUtils();
-      var result = testSweetsConfigUtils.deserializeConfigFileByKey(
-          testsweetFileContentRaw, ConfigFileKeyType.ProjectId);
+    group('TestSweetsConfigUtils functions -', () {
+      group('getStringValueFromMapByKey -', () {
+        test(
+            "when given a list of strings, Should split by '=' sign to List<MapEntry<String, String>>",
+            () async {
+          TestSweetsConfigUtils testSweetsConfigUtils = TestSweetsConfigUtils();
+          List<MapEntry<String, String>> result = testSweetsConfigUtils
+              .splittingStringOnEqualSign(testsweetFileContent);
+          expect(result[0].value, '3OezzTovG9xFTE5Xw2w1');
+        });
+      });
+      group('getStringValueFromMapByKey -', () {
+        test(
+            "When given testsweetFileContentMapEntries and ProjectId key, should return 3OezzTovG9xFTE5Xw2w1",
+            () async {
+          TestSweetsConfigUtils testSweetsConfigUtils = TestSweetsConfigUtils();
+          var result = testSweetsConfigUtils.getStringValueFromMapByKey(
+              testsweetFileContentListOfMapEntries,
+              ConfigFileKeyType.ProjectId);
+          expect(result, '3OezzTovG9xFTE5Xw2w1');
+        });
+      });
+      group('deserializeConfigFileByKey -', () {
+        test(
+            "When given testsweetFileContentRaw and ProjectId key, should return 3OezzTovG9xFTE5Xw2w1",
+            () async {
+          TestSweetsConfigUtils testSweetsConfigUtils = TestSweetsConfigUtils();
+          var result = testSweetsConfigUtils.deserializeConfigFileByKey(
+              testsweetFileContentRaw, ConfigFileKeyType.ProjectId);
 
-      expect(result, '3OezzTovG9xFTE5Xw2w1');
+          expect(result, '3OezzTovG9xFTE5Xw2w1');
+        });
+      });
     });
   });
 }
