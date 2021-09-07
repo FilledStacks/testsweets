44b76a0674ab024b1ac318b677d98ae152668939
Isaac Adeni Adariku <isaacadariku05@gmail.com>
2021-09-07 20:46:32 +0100

    Update widget_description_dialog_widget.dart

--- packages/testsweets/lib/src/ui/shared/widgets/widget_description_dialog_widget.dart
+++ packages/testsweets/lib/src/ui/shared/widgets/widget_description_dialog_widget.dart
@@ -17,42 +17,39 @@ class WidgetDescriptionDialog extends StatelessWidget {
     var positionY = description.position.y.toStringAsFixed(1);
     var widgetType = description.widgetType.toString().substring(11);
 
-    return Align(
-      alignment: Alignment.bottomCenter,
-      child: Container(
-        height: 250,
-        margin: EdgeInsets.only(bottom: 15, left: 12, right: 12),
-        padding: const EdgeInsets.symmetric(horizontal: 15),
-        child: SizedBox.expand(
-            child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text('Widget Description', style: boldStyle),
-                IconButton(
-                  icon: Icon(
-                    Icons.cancel,
-                    color: Colors.grey,
-                    size: 32,
-                  ),
-                  onPressed: onPressed,
-                ),
-              ],
+    return Container(
+      height: 250,
+      width: 390,
+      margin: EdgeInsets.symmetric(horizontal: 12),
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text('Widget Description', style: boldStyle),
+          IconButton(
+            icon: Icon(
+              Icons.cancel,
+              color: Colors.grey,
+              size: 32,
             ),
-            Text('View Name: ${description.viewName}', style: lightStyle),
-            Text('Name: ${description.name}', style: lightStyle),
-            Text('Widget Type: ${widgetType.capitalizeFirst}',
-                style: lightStyle),
-            Text('Position: (x:$positionX, y:$positionY)', style: lightStyle),
-          ],
-        )),
-        decoration: BoxDecoration(
-          color: Color(0xFF181920),
-          borderRadius: BorderRadius.circular(10),
-        ),
+            onPressed: onPressed,
+          ),
+        ],
+      ),
+      Text('View Name: ${description.viewName}', style: lightStyle),
+      Text('Name: ${description.name}', style: lightStyle),
+      Text('Widget Type: ${widgetType.capitalizeFirst}',
+          style: lightStyle),
+      Text('Position: (x:$positionX, y:$positionY)', style: lightStyle),
+        ],
+      ),
+      decoration: BoxDecoration(
+        color: Color(0xFF181920),
+        borderRadius: BorderRadius.circular(10),
       ),
     );
   }
