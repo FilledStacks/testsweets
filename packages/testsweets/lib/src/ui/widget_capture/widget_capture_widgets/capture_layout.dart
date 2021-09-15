32e7ea02f8e0fe27a891be527f61979598454184
Isaac Adeni Adariku <isaacadariku05@gmail.com>
2021-09-15 16:09:51 +0100

    update capture layout

--- packages/testsweets/lib/src/ui/widget_capture/widget_capture_widgets/capture_layout.dart
+++ packages/testsweets/lib/src/ui/widget_capture/widget_capture_widgets/capture_layout.dart
@@ -45,15 +45,21 @@ class CaptureLayout extends ViewModelWidget<WidgetCaptureViewModel> {
                   : Alignment.topCenter,
               widthFactor: 1,
               child: WidgetNameInput(
+                initialValue:
+                    model.isEditMode ? model.widgetDescription?.name : null,
+                onChanged: model.isEditMode ? model.onChangeWidgetName : null,
+                isEditMode: model.isEditMode,
                 errorMessage: model.nameInputErrorMessage,
                 closeWidget: () {
                   widgetNameController.clear();
                   model.closeWidgetNameInput();
                 },
+                deleteWidget: model.deleteWidgetDescription,
                 saveWidget: model.saveWidgetDescription,
                 switchPositionTap: model.switchWidgetNameInputPosition,
                 focusNode: widgetNameFocusNode,
-                textEditingController: widgetNameController,
+                textEditingController:
+                    model.isEditMode ? null : widgetNameController,
               ),
             ),
             isVisible: !model.isBusy &&
