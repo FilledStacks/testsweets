a18e78cb123a66d0df3301a5aea5f03a630c2c97
Isaac Adeni Adariku <isaacadariku05@gmail.com>
2021-09-22 22:15:42 +0100

    remove unused code

--- packages/testsweets/lib/testsweets.dart
+++ packages/testsweets/lib/testsweets.dart
@@ -5,12 +5,3 @@ export 'src/setup_code.dart';
 export 'src/ui/testsweets_navigator_observer.dart';
 export 'src/ui/testsweets_overlay/testsweets_overlay_view.dart';
 export 'src/ui/widget_capture/widget_capture_view.dart';
-
-// class TestSweets {
-//   /// Creates an tranparent overlay which can be turned on
-//   /// to inspect widgets and their key names
-//   static Widget builder(BuildContext context, Widget child,
-//       {bool enabled = true}) {
-//     return enabled && kDebugMode ? WidgetInspectorView(child: child) : child;
-//   }
-// }
