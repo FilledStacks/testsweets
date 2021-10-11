6a24784f1d90e7dbb77dd23ecce541f1f79d83a7
eb-so <ebsowork999@gmail.com>
2021-10-11 08:59:42 +0300

    redesign the black container switch postion buttone

--- packages/testsweets/lib/src/ui/widget_capture/widget_capture_widgets/black_wrapper_container.dart
+++ packages/testsweets/lib/src/ui/widget_capture/widget_capture_widgets/black_wrapper_container.dart
@@ -30,83 +30,102 @@ class BlackWrapperContainer extends ViewModelWidget<WidgetCaptureViewModel> {
 
   @override
   Widget build(BuildContext context, WidgetCaptureViewModel model) {
-    return SizedBox(
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 8.w).copyWith(
+          top: 32.h, bottom: MediaQuery.of(context).viewInsets.bottom + 24.h),
       width: ScreenUtil().screenWidth,
-      child: Padding(
-        padding: EdgeInsets.symmetric(
-          horizontal: 12.w,
-        ).copyWith(
-            top: 32.h, bottom: MediaQuery.of(context).viewInsets.bottom + 24.h),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            FadeInWidget(
-                child: Container(
-                  alignment: Alignment.center,
-                  width: ScreenUtil().screenWidth,
-                  padding: EdgeInsets.symmetric(vertical: 12, horizontal: 16.w),
-                  decoration: viewNameBoxDecoration,
-                  child: AutoSizeText(model.currentView,
-                      textAlign: TextAlign.center,
-                      style: tsNormalBold().copyWith(color: kcPrimaryWhite)),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          if (switchPositionTap != null)
+            RotatedBox(
+              quarterTurns: 1,
+              child: TextButton.icon(
+                onPressed: switchPositionTap,
+                style: ButtonStyle(
+                  tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                  fixedSize: MaterialStateProperty.all(Size(142.h, 22)),
+                  shape: MaterialStateProperty.all<OutlinedBorder>(
+                      RoundedRectangleBorder(
+                          borderRadius:
+                              BorderRadius.all(crTextFieldCornerRadius()))),
+                  backgroundColor: MaterialStateProperty.all(
+                      kcHighlightGrey.withOpacity(0.7)),
                 ),
-                isVisible: model.currentView.isNotEmpty),
-            Container(
-              padding: EdgeInsets.only(
-                  left: 16.w, right: 16.w, top: 12.h, bottom: 20.h),
-              decoration: blackBoxDecoration.copyWith(
-                  borderRadius: bottomCornerRaduisIsZero
-                      ? BorderRadius.vertical(top: crButtonCornerRadius())
-                      : BorderRadius.vertical(bottom: crButtonCornerRadius())),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      switchPositionTap != null
-                          ? TextButton.icon(
-                              onPressed: switchPositionTap,
-                              style: ButtonStyle(
-                                shape:
-                                    MaterialStateProperty.all<OutlinedBorder>(
-                                        RoundedRectangleBorder(
-                                            borderRadius: BorderRadius.all(
-                                                crTextFieldCornerRadius()))),
-                                backgroundColor: MaterialStateProperty.all(
-                                    kcSweetsAppBarColor),
-                              ),
-                              icon: Icon(
-                                Icons.swap_vert,
-                                size: 16.w,
-                                color: kcPrimaryWhite,
-                              ),
-                              label: AutoSizeText('Switch Position',
-                                  maxLines: 1,
-                                  style: tsNormal().copyWith(
-                                      color: Colors.white, fontSize: 14.w)),
-                            )
-                          : title != null
-                              ? Expanded(child: Text(title!, style: boldStyle))
-                              : const SizedBox(),
-                      closeWidgetOnTap != null
-                          ? CloseCircularButton(onTap: closeWidgetOnTap!)
-                          : const SizedBox()
-                    ],
+                icon: RotatedBox(
+                  quarterTurns: 1,
+                  child: Icon(
+                    Icons.swap_vert,
+                    size: 16.w,
+                    color: kcPrimaryWhite,
                   ),
-                  SizedBox(
-                    height: spaceBetweenTopControllersAndChild ?? 24.w,
-                  ),
-                  child,
-                ],
+                ),
+                label: AutoSizeText('Switch Position',
+                    maxLines: 1,
+                    style: tsNormal()
+                        .copyWith(color: Colors.white, fontSize: 14.w)),
               ),
             ),
-            footerChild ?? const SizedBox()
-          ],
-        ),
+          SizedBox(
+            width: 4.w,
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                FadeInWidget(
+                    child: Container(
+                      alignment: Alignment.center,
+                      padding:
+                          EdgeInsets.symmetric(vertical: 12, horizontal: 16.w),
+                      decoration: viewNameBoxDecoration,
+                      child: Row(
+                        children: [
+                          AutoSizeText(model.currentView,
+                              textAlign: TextAlign.center,
+                              style: tsNormalBold()
+                                  .copyWith(color: kcPrimaryWhite)),
+                        ],
+                      ),
+                    ),
+                    isVisible: model.currentView.isNotEmpty),
+                Container(
+                  padding: EdgeInsets.only(
+                      left: 16.w, right: 16.w, top: 4.h, bottom: 20.h),
+                  decoration: blackBoxDecoration.copyWith(
+                      borderRadius: bottomCornerRaduisIsZero
+                          ? BorderRadius.vertical(top: crButtonCornerRadius())
+                          : BorderRadius.vertical(
+                              bottom: crButtonCornerRadius())),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          title != null
+                              ? Expanded(child: Text(title!, style: boldStyle))
+                              : const SizedBox(),
+                          closeWidgetOnTap != null
+                              ? CloseCircularButton(onTap: closeWidgetOnTap!)
+                              : const SizedBox()
+                        ],
+                      ),
+                      SizedBox(
+                        height: spaceBetweenTopControllersAndChild ?? 24.w,
+                      ),
+                      child,
+                    ],
+                  ),
+                ),
+                footerChild ?? const SizedBox()
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
