82c3ef46082938f49d12966e77878e40ca3c7f94
Dane Mackier <dane.mackier@outlook.com>
2021-10-12 11:32:47 +0200

    updates project id

--- packages/testsweets/example/lib/main.dart
+++ packages/testsweets/example/lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       builder: (context, child) => TestSweetsOverlayView(
-        projectId: '7lyZJXDJQtLodBvJsYsu',
+        projectId: '4BwobHaZRWjMn1hHdgqv',
         child: child!,
         captureWidgets: !FLUTTER_DRIVER,
       ),
