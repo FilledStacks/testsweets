81d974ecbdcf5fa203b388dbd8b8a406d8dc267f
Ebrahim soliman <soliman.ebrahim.dev@gmail.com>
2022-03-30 17:24:55 +0300

    wrap the extractedScrollableDescriptions with try-catch

--- packages/testsweets/lib/src/ui/shared/find_scrollables.dart
+++ packages/testsweets/lib/src/ui/shared/find_scrollables.dart
@@ -26,25 +26,38 @@ class FindScrollablesImp implements FindScrollables {
       return [];
     }
     log.v('foundedElements: $foundedElements');
-    final extractedScrollableDescriptions = foundedElements!.map((item) {
-      RenderBox renderBox = item.findRenderObject() as RenderBox;
+    final extractedScrollableDescriptions = foundedElements!
+        .map((item) {
+          try {
+            RenderBox renderBox = item.findRenderObject() as RenderBox;
 
-      Offset globalPostion = renderBox.localToGlobal(Offset.zero);
-      final position = (item.widget as Scrollable).controller!.positions.first;
+            Offset globalPostion = renderBox.localToGlobal(Offset.zero);
+            final position =
+                (item.widget as Scrollable).controller!.positions.first;
 
-      final scrollExtentByPixels = position.pixels;
-      final maxScrollExtentByPixels = position.maxScrollExtent;
-      final axis = position.axis;
-      final rect = SerializableRect.fromPoints(globalPostion,
-          globalPostion.translate(renderBox.size.width, renderBox.size.height));
+            final scrollExtentByPixels = position.pixels;
+            final maxScrollExtentByPixels = position.maxScrollExtent;
+            final axis = position.axis;
+            final rect = SerializableRect.fromPoints(
+                globalPostion,
+                globalPostion.translate(
+                    renderBox.size.width, renderBox.size.height));
 
-      return ScrollableDescription(
-          axis: axis,
-          rect: rect,
-          scrollExtentByPixels: scrollExtentByPixels,
-          maxScrollExtentByPixels: maxScrollExtentByPixels);
-    });
+            return ScrollableDescription(
+                axis: axis,
+                rect: rect,
+                scrollExtentByPixels: scrollExtentByPixels,
+                maxScrollExtentByPixels: maxScrollExtentByPixels);
+          } catch (e) {
+            log.e(e);
+            return null;
+          }
+        })
+        .where((element) => element != null)
+        .cast<ScrollableDescription>();
+
     log.i('extractedScrollableDescriptions: $extractedScrollableDescriptions');
+
     return extractedScrollableDescriptions;
   }
 }
