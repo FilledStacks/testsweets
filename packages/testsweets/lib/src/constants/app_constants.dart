9de61765d5d77bd02911a7655b607f84d5556d7e
Ebrahim soliman <soliman.ebrahim.dev@gmail.com>
2022-04-05 10:18:16 +0300

    TEST_INTEGRITY_TIMEOUT to 25 sec

--- packages/testsweets/lib/src/constants/app_constants.dart
+++ packages/testsweets/lib/src/constants/app_constants.dart
@@ -1,3 +1,3 @@
 const double WIDGET_DESCRIPTION_VISUAL_SIZE = 42.0;
 const double SCROLLABLE_DETECTION_FORGIVENESS = 10.0;
-const TEST_INTEGRITY_TIMEOUT = const Duration(seconds: 30);
+const TEST_INTEGRITY_TIMEOUT = const Duration(seconds: 25);
