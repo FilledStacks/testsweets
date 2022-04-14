731c33f479be16c8dddf2ac277faaa833cf6c35d
Ebrahim soliman <soliman.ebrahim.dev@gmail.com>
2022-04-14 08:28:40 +0300

    - Remove deprecate code from testroutetracker - Put all the setRoute code inside a postframecallback

--- packages/testsweets/lib/src/services/testsweets_route_tracker.dart
+++ packages/testsweets/lib/src/services/testsweets_route_tracker.dart
@@ -4,20 +4,20 @@ import 'package:testsweets/src/extensions/string_extension.dart';
 
 class TestSweetsRouteTracker extends ChangeNotifier {
   final log = getLogger('TestSweetsRouteTracker');
+
   @visibleForTesting
   bool testMode = false;
-  static TestSweetsRouteTracker? _instance;
   @visibleForTesting
   Map<String, int> indexedRouteStateMap = {};
+
+  static TestSweetsRouteTracker? _instance;
+
   static TestSweetsRouteTracker get instance {
     if (_instance == null) {
       _instance = TestSweetsRouteTracker();
     }
     return _instance!;
   }
-
-  bool get isChildRouteActivated => _tempRoute.isEmpty;
-  bool get isNestedView => _tempRoute.isNotEmpty || _parentRoute.isNotEmpty;
 
   String previosRoute = '';
   String _currentRoute = '';
@@ -26,64 +26,28 @@ class TestSweetsRouteTracker extends ChangeNotifier {
       ? _currentRoute.convertViewNameToValidFormat
       : '';
 
-  String _parentRoute = '';
-  String get parentRoute => _parentRoute;
-  String get formatedParentRoute =>
-      _parentRoute.isNotEmpty ? _parentRoute.convertViewNameToValidFormat : '';
-
-  String _tempRoute = '';
-  String get formatedTempRoute =>
-      _tempRoute.isNotEmpty ? _tempRoute.convertViewNameToValidFormat : '';
-
-  String get leftViewName => isNestedView
-      ? isChildRouteActivated
-          ? formatedParentRoute
-          : formatedCurrentRoute
-      : '';
-
-  String get rightViewName => isChildRouteActivated
-      ? formatedCurrentRoute
-      : _tempRoute.convertViewNameToValidFormat;
-
   void setCurrentRoute(String route) {
-    log.i('setCurrentRoute | route: $route');
-    _parentRoute = '';
-    _tempRoute = '';
+    log.i('route: $_currentRoute | previousRoute: $previosRoute');
+
+    if (testMode) {
+      setRoute(route);
+      return;
+    }
+
+    WidgetsBinding.instance!.addPostFrameCallback((_) {
+      setRoute(route);
+      notifyListeners();
+    });
+  }
+
+  void setRoute(String route) {
     previosRoute = _currentRoute;
     _currentRoute = route;
     loadRouteIndexIfExist(route);
-    refreshUi();
-  }
-
-  void setparentRoute(String route) {
-    log.i('setparentRoute | parentRoute: $route');
-    _parentRoute = route;
-    refreshUi();
-  }
-
-  void toggleActivatedRouteBetweenParentAndChild() {
-    if (_tempRoute.isEmpty) {
-      _tempRoute = _currentRoute;
-      _currentRoute = _parentRoute;
-      _parentRoute = '';
-    } else {
-      _parentRoute = _currentRoute;
-      _currentRoute = _tempRoute;
-      _tempRoute = '';
-    }
-    refreshUi();
-    log.v(
-        'tempRoute: $_tempRoute | currentRoute: $_currentRoute | parentRoute: $_parentRoute');
-  }
-
-  void refreshUi() {
-    if (!testMode)
-      WidgetsBinding.instance!.addPostFrameCallback((_) => notifyListeners());
   }
 
   void changeRouteIndex(String viewName, int index) {
     setCurrentRoute(viewName + index.toString());
-    setparentRoute(viewName);
     saveRouteIndex(viewName, index);
   }
 
@@ -94,7 +58,6 @@ class TestSweetsRouteTracker extends ChangeNotifier {
   void loadRouteIndexIfExist(String viewName) {
     if (indexedRouteStateMap.containsKey(viewName)) {
       _currentRoute = viewName + indexedRouteStateMap[viewName].toString();
-      _parentRoute = viewName;
     }
   }
 }
